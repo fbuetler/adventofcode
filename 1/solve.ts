d2feee2b894a6643941838a8f357b120c17d42fb
Florian Buetler <flbuetle@ethz.ch>
2020-12-01 13:18:28 +0100

    improve complexity of 1.12

--- 1/solve.ts
+++ 1/solve.ts
@@ -1,38 +1,54 @@
 import { readFileSync } from "fs";
 
-function solvePartOne(input: string[]) {
+function solvePartOne(input: number[]) {
   console.log("part 1");
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      if (+input[i] + +input[j] === 2020) {
-        console.log(`${input[i]} * ${input[j]} = ${+input[i] * +input[j]}`);
-        return;
-      }
+  let left = 0;
+  let right = input.length - 1;
+  while (left < right) {
+    const a = input[left];
+    const b = input[right];
+    if (a + b === 2020) {
+      console.log(`${a} * ${b} = ${a * b}`);
+      return;
+    } else if (a + b > 2020) {
+      right--;
+    } else {
+      left++;
     }
   }
 }
 
-function solvePartTwo(input: string[]) {
+function solvePartTwo(input: number[]) {
   console.log("part 2");
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      for (let k = 0; k < input.length; k++) {
-        if (+input[i] + +input[j] + +input[k] === 2020) {
-          console.log(
-            `${+input[i]} * ${+input[j]} * ${+input[k]} = ${
-              +input[i] * +input[j] * +input[k]
-            }`
-          );
-          return;
-        }
+  let completed = false;
+  for (let i = 0; i < input.length - 1; i++) {
+    const a = input[i];
+    let left = i + 1;
+    let right = input.length - 1;
+    while (left < right) {
+      const b = input[left];
+      const c = input[right];
+      if (a + b + c === 2020) {
+        completed = true;
+        console.log(`${a} * ${b} * ${c} = ${a * b * c}`);
+        break;
+      } else if (a + b + c > 2020) {
+        right--;
+      } else {
+        left++;
       }
     }
+    if (completed) {
+      return;
+    }
   }
-  return;
 }
 
 const data = readFileSync("./input.txt", "utf8");
-const input = data.split("\n");
+const input = data
+  .split("\n")
+  .map((el) => +el)
+  .sort((a, b) => a - b);
 
 solvePartOne(input);
 solvePartTwo(input);
