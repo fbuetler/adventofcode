8c396d65501bfcf678e44ea522c2966714c0b550
Florian Buetler <flbuetle@ethz.ch>
2020-12-26 01:36:06 +0100

    solve puzzle part 2 of 20.12

--- 20/solve.ts
+++ 20/solve.ts
@@ -1,217 +1,197 @@
 import { readFileSync } from "fs";
 
-enum Direction {
-  LEFT = "LEFT",
-  RIGHT = "RIGHT",
-  UP = "UP",
-  DOWN = "DOWN",
-}
-
-enum Transformation {
-  ORIGINAL = "ORIGINAL",
-  VERTICAL = "VERTICAL",
-  HORIZONTAL = "HORIZONTAL",
-  ROTATION90 = "ROTATION90",
-  ROTATION180 = "ROTATION180",
-  ROTATION270 = "ROTATION270",
-}
-
 class Photo {
   tiles = new Map<number, Tile>();
-  reconstructed = new Array<Array<number>>();
+  reconstructed = new Map<string, Tile>();
   size: number;
+  edgeCount = new Map<string, number>();
+  photo: Tile;
+  monster = [
+    "..................#.",
+    "#....##....##....###",
+    ".#..#..#..#..#..#...",
+  ];
 
   constructor(rawTiles: string[][]) {
     rawTiles.forEach((tile) => {
       const title = tile.shift();
-      this.tiles.set(+title.replace(":", "").split(" ")[1], new Tile(tile));
+      const id = +title.replace(":", "").split(" ")[1];
+      this.tiles.set(id, new Tile(tile));
+      // count edge appearances because border edge only appear once
+      this.tiles
+        .get(id)
+        .edges()
+        .forEach((edge) => {
+          this.edgeCount.set(edge, (this.edgeCount.get(edge) || 0) + 1);
+          this.edgeCount.set(
+            edge.split("").reverse().join(""),
+            (this.edgeCount.get(edge.split("").reverse().join("")) || 0) + 1
+          );
+        });
     });
     this.size = Math.sqrt(rawTiles.length);
-    for (let i = 0; i < this.size; i++) {
-      this.reconstructed.push(new Array<number>(this.size));
-    }
+    this.photo = new Tile(
+      new Array<string>(this.size * (rawTiles[0].length - 2)).fill("")
+    );
   }
 
   reconstruct() {
-    const neighbours = new Map<number, Set<number>>();
-    const tileKeys = Array.from(this.tiles.keys()).sort();
-    for (let tileKey of tileKeys) {
-      for (let otherTileKey of tileKeys) {
-        if (tileKey === otherTileKey) {
-          continue;
-        }
-        const tile = this.tiles.get(tileKey);
-        const otherTile = this.tiles.get(otherTileKey);
-        const transformations = [
-          Transformation.ORIGINAL,
-          Transformation.HORIZONTAL,
-          Transformation.VERTICAL,
-          Transformation.ROTATION90,
-          Transformation.ROTATION180,
-          Transformation.ROTATION270,
-        ];
-        [Direction.LEFT, Direction.RIGHT, Direction.UP, Direction.DOWN].forEach(
-          (direction: Direction) => {
-            for (let i = 0; i < transformations.length; i++) {
-              for (let j = 0; j < transformations.length; j++) {
-                if (
-                  Tile.fits(
-                    tile,
-                    otherTile,
-                    direction,
-                    transformations[i],
-                    transformations[j]
-                  )
-                ) {
-                  neighbours.set(
-                    tileKey,
-                    (neighbours.get(tileKey) || new Set<number>()).add(
-                      otherTileKey
-                    )
-                  );
-                }
+    let cornerProduct = 1;
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        let found = false;
+        for (let id of Array.from(this.tiles.keys())) {
+          const tile = this.tiles.get(id);
+          for (let transformation of Tile.transformations(tile)) {
+            const topEdge = transformation.rows[0];
+            const leftEdge = transformation.col(0);
+            /*
+                is top row OR top edge fits to upper tile AND
+                is leftmost col OR left edge fits to left tile
+              */
+            if (
+              ((row === 0 && this.edgeCount.get(topEdge) === 1) ||
+                (row !== 0 &&
+                  topEdge ===
+                    this.reconstructed.get(`${col},${row - 1}`).rows[
+                      tile.size - 1
+                    ])) &&
+              ((col === 0 && this.edgeCount.get(leftEdge) === 1) ||
+                (col !== 0 &&
+                  leftEdge ===
+                    this.reconstructed
+                      .get(`${col - 1},${row}`)
+                      .col(tile.size - 1)))
+            ) {
+              // is corner
+              if (
+                (row === 0 || row === this.size - 1) &&
+                (col === 0 || col === this.size - 1)
+              ) {
+                cornerProduct *= id;
               }
+
+              // reconstruct the photo
+              for (let i = 0; i < tile.size - 2; i++) {
+                this.photo.rows[
+                  (tile.size - 2) * row + i
+                ] += transformation.rows[i + 1].substring(1, tile.size - 1);
+              }
+
+              this.reconstructed.set(`${col},${row}`, transformation);
+              this.tiles.delete(id);
+              found = true;
+              break;
             }
           }
-        );
+          if (found) {
+            break;
+          }
+        }
       }
     }
-    console.log(neighbours);
-    let randomCorner: number;
-    let cornerProduct = 1;
-    neighbours.forEach((nbrs, tileKey) => {
-      cornerProduct *= nbrs.size === 2 ? tileKey : 1;
-      randomCorner = tileKey;
-    });
     console.log(`Part 1: ${cornerProduct}`);
+  }
+
+  waterRoughness(): number {
+    let monsters = 0;
+    for (let transformation of Tile.transformations(this.photo)) {
+      for (let row = 0; row < transformation.rows.length; row++) {
+        for (let col = 0; col < transformation.rows[0].length; col++) {
+          if (this.partContainsMonster(transformation, row, col)) {
+            monsters++;
+          }
+        }
+      }
+    }
+    let hashesInPhoto = 0;
+    this.photo.rows.forEach(
+      (row) => (hashesInPhoto += (row.match(new RegExp("#", "g")) || []).length)
+    );
+    let hashesInMonster = 0;
+    this.monster.forEach(
+      (row) =>
+        (hashesInMonster += (row.match(new RegExp("#", "g")) || []).length)
+    );
+    return hashesInPhoto - monsters * hashesInMonster;
+  }
+
+  partContainsMonster(tile: Tile, row: number, col: number): boolean {
+    if (
+      row + this.monster.length > tile.rows.length ||
+      col + this.monster[0].length > tile.rows[0].length
+    ) {
+      return false;
+    }
+    for (let i = 0; i < this.monster.length; i++) {
+      for (let j = 0; j < this.monster[i].length; j++) {
+        if (
+          this.monster[i][j] === "#" &&
+          tile.rows[row + i][col + j] !== this.monster[i][j]
+        ) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  toString(): string {
+    return this.photo.toString();
   }
 }
 
 class Tile {
-  tile: string[];
+  rows: string[];
+  size: number;
 
-  constructor(tile: string[]) {
-    this.tile = tile;
+  constructor(rows: string[]) {
+    this.rows = rows;
+    this.size = rows.length;
   }
 
-  rotate90() {
-    let newTile = new Array<string>(this.tile.length);
-    for (let i = 0; i < this.tile.length; i++) {
-      let row = "";
-      for (let j = 0; j < this.tile.length; j++) {
-        row = row.concat(this.tile[this.tile.length - 1 - j][i]);
-      }
-      newTile[i] = row;
-    }
-    this.tile = newTile;
+  col(index: number): string {
+    let col = "";
+    this.rows.forEach((row) => (col += row[index]));
+    return col;
   }
 
-  flipVertically() {
-    let newTile = new Array<string>(this.tile.length);
-    for (let i = 0; i < this.tile.length; i++) {
-      newTile[i] = this.tile[i].split("").reverse().join("");
-    }
-    this.tile = newTile;
-  }
-
-  flipHorrizontally() {
-    let newTile = new Array<string>(this.tile.length);
-    for (let i = 0; i < this.tile.length; i++) {
-      newTile[this.tile.length - 1 - i] = this.tile[i];
-    }
-    this.tile = newTile;
-  }
-
-  static fits(
-    one: Tile,
-    other: Tile,
-    direction: Direction,
-    tf: Transformation,
-    otherTf: Transformation
-  ): boolean {
-    const copy = one.copy();
-    const otherCopy = other.copy();
-    const config: [Tile, Transformation][] = [
-      [copy, tf],
-      [otherCopy, otherTf],
+  edges(): string[] {
+    return [
+      this.rows[0],
+      this.col(this.size - 1),
+      this.rows[this.size - 1],
+      this.col(0),
     ];
-
-    for (let [t, transformation] of config) {
-      switch (transformation) {
-        case Transformation.ORIGINAL: {
-          break;
-        }
-        case Transformation.VERTICAL: {
-          t.flipVertically();
-          break;
-        }
-        case Transformation.HORIZONTAL: {
-          t.flipHorrizontally();
-          break;
-        }
-        case Transformation.ROTATION90: {
-          t.rotate90();
-          break;
-        }
-        case Transformation.ROTATION180: {
-          t.rotate90();
-          t.rotate90();
-          break;
-        }
-        case Transformation.ROTATION270: {
-          t.rotate90();
-          t.rotate90();
-          t.rotate90();
-          break;
-        }
-      }
-    }
-
-    if (copy.checkTransformation(otherCopy, direction)) {
-      one = copy;
-      other = otherCopy;
-      return true;
-    }
-    return false;
   }
 
-  checkTransformation(other: Tile, direction: Direction): boolean {
-    switch (direction) {
-      case Direction.LEFT: {
-        for (let i = 0; i < this.tile.length; i++) {
-          if (this.tile[i].substr(0, 1) !== other.tile[i].substr(-1, 1)) {
-            return false;
-          }
-        }
-        return true;
+  static transformations(t: Tile): Tile[] {
+    const size = t.size;
+    const trans = new Array<Tile>();
+    for (let i = 0; i < 8; i += 2) {
+      let newTile = new Array<string>();
+      // flip vertically
+      for (let j = 0; j < size; j++) {
+        newTile.push(t.rows[j].split("").reverse().join(""));
       }
-      case Direction.RIGHT: {
-        for (let i = 0; i < this.tile.length; i++) {
-          if (this.tile[i].substr(-1, 1) !== other.tile[i].substr(0, 1)) {
-            return false;
-          }
-        }
-        return true;
+      trans.push(new Tile(newTile));
+      newTile = new Array<string>();
+      // rotate
+      for (let j = 0; j < size; j++) {
+        newTile.push(t.col(j).split("").reverse().join(""));
       }
-      case Direction.UP: {
-        return this.tile[0] === other.tile[0];
-      }
-      case Direction.DOWN: {
-        const len = this.tile.length - 1;
-        return this.tile[len] === other.tile[len];
-      }
-      default:
-        return false;
+      trans.push(new Tile(newTile));
+      // update
+      t = trans[i + 1];
     }
-  }
-
-  copy(): Tile {
-    return new Tile(this.tile.slice());
+    return trans;
   }
 
   toString(): string {
     let str = "";
-    this.tile.forEach((line) => (str += `${line}\n`));
+    for (let i = 0; i < this.rows.length; i++) {
+      str += `${this.rows[i]}\n`;
+    }
     return str;
   }
 }
@@ -222,3 +202,4 @@ input.forEach((el) => tiles.push(el.split("\n")));
 
 const photo = new Photo(tiles);
 photo.reconstruct();
+console.log(`Part 2: ${photo.waterRoughness()}`);
