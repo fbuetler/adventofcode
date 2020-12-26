b0b5335873ff21e00a6451a77dbffdd2bca71ac2
Florian Buetler <flbuetle@ethz.ch>
2020-12-26 03:17:43 +0100

    solve puzzle part 2 of 16.12

--- 16/solve.ts
+++ 16/solve.ts
@@ -60,3 +60,52 @@ for (let i = 0; i < nearbyTickets.length; i++) {
 }
 console.log(`Part 1: ${errorRate}`);
 
+const possibleRanges = new Map<string, Set<number>>();
+fields.forEach((val, key) =>
+  possibleRanges.set(
+    key,
+    new Set<number>(
+      Array(validTickets[0].length)
+        .fill(0)
+        .map((_, i) => i)
+    )
+  )
+);
+for (let ticket of validTickets) {
+  let index = 0;
+  for (let field of ticket) {
+    for (let [name, range] of Array.from(fields)) {
+      if (
+        !isValidField(field, range[0][0], range[0][1], range[1][0], range[1][1])
+      ) {
+        const pr = possibleRanges.get(name);
+        pr.delete(index);
+        possibleRanges.set(name, pr);
+      }
+    }
+    index++;
+  }
+}
+
+const assigned = new Map<string, number>();
+for (let i = 0; i < possibleRanges.size; i++) {
+  let onePossibility: string;
+  possibleRanges.forEach((possibilities, field) => {
+    if (possibilities.size === 1) {
+      onePossibility = field;
+    }
+  });
+  const category = Array.from(possibleRanges.get(onePossibility))[0];
+  assigned.set(onePossibility, category);
+  possibleRanges.forEach((possibilities) => {
+    possibilities.delete(category);
+  });
+}
+
+let product = 1;
+assigned.forEach((index, el) => {
+  if (el.startsWith("departure")) {
+    product *= myTicket[index];
+  }
+});
+console.log(`Part 2: ${product}`);
