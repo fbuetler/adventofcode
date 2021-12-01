d5190fed71636f3bb7d357afff6b3dc60005e62b
Florian Buetler <flbuetle@ethz.ch>
2021-12-01 17:48:03 +0100

    add go template

---
+++ 2021/template/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+
+}
+
+func readInput(fileName string) []string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	input := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return input
+}
