9f82f94fc2daaa82db3d4db53dacd495d2d56d0d
Florian Bütler <flbuetle@ethz.ch>
2024-12-02 10:24:34 +0100

    solve puzzle of 1.12

---
+++ 2024/template/solve.py
@@ -0,0 +1,26 @@
+from aocd import submit
+
+def parse(lines):
+    return
+
+def part_a(input):
+    pass
+
+def part_b(input):
+    pass
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = [line.rstrip() for line in f]
+
+    input = parse(lines)
+
+    a = part_a(input)
+    if a:
+        print(a)
+        submit(a, part="a")
+
+    b = part_b(input)
+    if b:
+        print(b)
+        submit(b,  part="b")
