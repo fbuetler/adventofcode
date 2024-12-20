ff123b865d0437a5175505a97272f8ad1385894b
Florian Bütler <florian.buetler@hotmail.com>
2024-12-20 22:35:09 +0100

    solve 12.12 differently

--- 2024/12/solve.py
+++ 2024/12/solve.py
@@ -2,6 +2,8 @@ from copy import deepcopy
 
 from aocd import submit
 
+DIRS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
 
 def parse(lines):
     map = [list(l) for l in lines]
@@ -39,7 +41,7 @@ def part_b(input):
         while len(plant_locs) != 0:
             region = connected_plants_bfs(map, plant_type, plant_locs[0])
             area = len(region)
-            sides = all_sides(map, plant_type, region)
+            sides = corners(map, plant_type, region)  # corners == sides
             sum += area * sides
             for r in region:
                 plant_locs.remove(r)
@@ -47,28 +49,27 @@ def part_b(input):
 
 
 def connected_plants_bfs(map, plant_type, s):
-    visited = [[False for _ in range(len(map[0]))] for _ in range(len(map))]
+    visited = {s}
     q = [s]
-    visited[s[0]][s[1]] = True
 
     while len(q) != 0:
         i, j = q.pop(0)
-        for (u, v) in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+        for (u, v) in DIRS:
             a = i + u
             b = j + v
             if a < 0 or a >= len(map) or b < 0 or b >= len(map[0]):
                 continue
-            if not visited[a][b] and map[a][b] == plant_type:
-                visited[a][b] = True
+            if (a, b) not in visited and map[a][b] == plant_type:
+                visited.add((a, b))
                 q.append((a, b))
 
-    return [(i, j) for j in range(len(visited[0])) for i in range(len(visited)) if visited[i][j]]
+    return list(visited)
 
 
 def fences(map, plant_type, region):
     f = 0
     for i, j in region:
-        for (u, v) in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
+        for (u, v) in DIRS:
             a = i + u
             b = j + v
             if a < 0 or a >= len(map) or b < 0 or b >= len(map[0]):
@@ -78,74 +79,29 @@ def fences(map, plant_type, region):
     return f
 
 
-def all_sides(m, plant_type, region: list):
-    return sides_horizontal(m, plant_type, region) + sides_vertical(m, plant_type, region)
+def corners(map, plant_type, region: list):
+    c = 0
+    n = len(DIRS)
 
+    def is_plant(x, y):
+        if x < 0 or x >= len(map) or y < 0 or y >= len(map[0]):
+            return False
+        return map[x][y] == plant_type
 
-def sides_horizontal(m, plant_type, region):
-    min_x = min(list(map(lambda x: x[0], region)))
-    max_x = max(list(map(lambda x: x[0], region)))
+    for px, py in sorted(region):
+        for i in range(n):
+            x1, y1 = DIRS[i]
+            x2, y2 = DIRS[(i+1) % n]
+            if not is_plant(px+x1, py+y1) and not is_plant(px+x2, py+y2):
+                # outside corners
+                c += 1
 
-    # scan horizontal sides
-    sides_line = 0
-    for i in range(min_x, max_x+1, 1):
-        # go line by line
-        line = list(filter(lambda x: x[0] == i, region))
+            dx, dy = x1 + x2, y1 + y2
+            if is_plant(px+x1, py+y1) and is_plant(px+x2, py+y2) and not is_plant(px+dx, py+dy):
+                # inside corners
+                c += 1
 
-        # find borders
-        up = list()
-        down = list()
-        for u, v in line:
-            if u == 0 or m[u-1][v] != plant_type:
-                # border left/right
-                up.append(v)
-            if i == len(m) - 1 or m[u+1][v] != plant_type:
-                # neighbouring another plant type up/down
-                down.append(v)
-
-        # sum up sides facing up/down
-        sides_line += sides_facing(up) + sides_facing(down)
-
-    return sides_line
-
-
-def sides_vertical(m, plant_type, region):
-    min_y = min(list(map(lambda x: x[1], region)))
-    max_y = max(list(map(lambda x: x[1], region)))
-
-    # scan vertical sides
-    sides_col = 0
-    for i in range(min_y, max_y+1, 1):
-        # go column by column
-        col = list(filter(lambda x: x[1] == i, region))
-
-        # find borders
-        left = list()
-        right = list()
-        for u, v in col:
-            if v == 0 or m[u][v-1] != plant_type:
-                # border left/right
-                left.append(u)
-            if i == len(m[0]) - 1 or m[u][v+1] != plant_type:
-                # neighbouring another plant type left/right
-                right.append(u)
-
-        # sum up sides facing left/right
-        sides_col += sides_facing(left) + sides_facing(right)
-
-    return sides_col
-
-
-def sides_facing(l):
-    s = 0
-    for j in range(len(l)):
-        if j == 0:
-            # fence-post
-            s += 1
-        elif l[j] - l[j-1] != 1:
-            # neighbouring
-            s += 1
-    return s
+    return c
 
 
 if __name__ == "__main__":
